2232ec239b48cb9994e36cd2f1a420fde94d3c9a
Sameed <sameedhassan22@gmail.com>
2023-11-30 22:52:54 +0500

    img changes in index

--- index.php
+++ index.php
@@ -4,44 +4,45 @@ include_once("include/index-navbar.php")
 <!-- =========================
         Slider Section
     ============================== -->
-<section id="main-slider-section">
-	<div id="main-slider" class="slider-bg2  owl-carousel owl-theme product-review slider-cat">
-		<div class="item d-flex  slider-bg align-items-center" style="background-image: url(img/slider-img/headphone.jpg);">
-			<div class="container-fluid">
-				<div class="row justify-content-end">
-					<div class="slider-text col-sm-6  col-xl-4   col-md-6 order-2 order-sm-1">
-						<h6 class="sub-title">Choose your favourite market</h6>
-						<h1 class="slider-title"><strong class="highlights-text">Compare</strong> Best Prices</h1>
-						<p class="slider-content">Grabe it hurry.</p>
-						<a href="shop-left-sidebar.html" class="btn btn-primary wd-shop-btn slider-btn">
-							Go to store <i class="fa fa-arrow-right" aria-hidden="true"></i>
-						</a>
-					</div>
-					<div class="col-sm-6 col-md-6 col-xl-6 order-1 order-sm-2 slider-img">
-						<img src="img/slider-img/watch.jpg" alt="" class="rounded">
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="item d-flex  slider-bg align-items-center" style="background-image: url(img/slider-img/shoes-bg.jpg);">
-			<div class="container-fluid">
-				<div class="row justify-content-end">
-					<div class="slider-text  col-sm-6 col-xl-4    col-md-6">
-						<h6 class="sub-title">Choose your favourite market</h6>
-						<h1 class="slider-title"><strong class="highlights-text">Compare</strong> Best Prices</h1>
-						<p class="slider-content">Grabe it hurry.</p>
-						<a href="shop-left-sidebar.html" class="btn btn-primary wd-shop-btn slider-btn">
-							Go to store <i class="fa fa-arrow-right" aria-hidden="true"></i>
-						</a>
-					</div>
-					<div class="col-sm-6 col-md-6 col-xl-6 mt-2 slider-img fadeInDown animated">
-						<img src="img/slider-img/shoes.jpg" alt="" class="rounded">
+	<section id="main-slider-section">
+    	<div id="main-slider" class="slider-bg2  owl-carousel owl-theme product-review slider-cat">
+			<div class="item d-flex  slider-bg align-items-center">
+				<div class="container-fluid">
+					<div class="row justify-content-end">
+					
+						<div class="slider-text col-sm-6  col-xl-4   col-md-6 order-2 order-sm-1">
+							<h6 class="sub-title">Choose your favourite market</h6>
+							<h1 class="slider-title"><strong class="highlights-text">Compare</strong> Best Prices</h1>
+							<p class="slider-content">Grabe it hurry.</p>
+							<a href="shop-left-sidebar.html" class="btn btn-primary wd-shop-btn slider-btn">
+								Go to store <i class="fa fa-arrow-right" aria-hidden="true"></i>
+							</a>
+						</div>
+						<div class="col-sm-6  col-md-6 col-xl-6 order-1 order-sm-2 slider-img">
+						    <img src="img/slider-img/slider2.png" alt="">
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-</section>
+           <div class="item d-flex  slider-bg align-items-center">
+				<div class="container-fluid">
+					<div class="row justify-content-end">
+						<div class="slider-text  col-sm-6 col-xl-4    col-md-6">
+							<h6 class="sub-title">Choose your favourite market</h6>
+							<h1 class="slider-title"><strong class="highlights-text">Compare</strong> Best Prices</h1>
+							<p class="slider-content">Grabe it hurry.</p>
+							<a href="shop-left-sidebar.html" class="btn btn-primary wd-shop-btn slider-btn">
+								Go to store <i class="fa fa-arrow-right" aria-hidden="true"></i>
+							</a>
+						</div>
+						<div class="col-sm-6 col-md-6 col-xl-6  slider-img fadeInDown animated">
+						    <img src="img/slider-img/slider4.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+    	</div>
+    </section>
 <!-- =========================
         Service  Section
     ============================== -->
